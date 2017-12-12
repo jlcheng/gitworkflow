e4b154e969485d48eb4e12ddb3f7bd30c8e1d8ec
John Cheng <johnlicheng@gmail.com>
2017-12-12 13:05:07 -0800

    Introduce interleaving commits into rebase workflow

--- rebase/success/workflow.sh
+++ rebase/success/workflow.sh
@@ -1,29 +1,38 @@
-qsource=origin
-fork=fork
 start=`pwd`
+origin="$start/origin"
+fork="$start/fork"
 
-rm -rf $source $fork
-mkdir $source
-cd $source
+rm -rf $origin $fork
+mkdir $origin
+cd $origin
 touch .gitignore
 git init
 git add -A
 git commit -m "init"
 
 cd $start
-git clone $source $fork
+git clone $origin $fork
 
-cd $start
-cd $source
+cd $origin
 touch 1.txt
 git add 1.txt
 git commit -m "add 1.txt"
 
-cd $start
 cd $fork
 touch 2.txt
 git add 2.txt
 git commit -m "add 2.txt"
+
+cd $origin
+touch 3.txt
+git add 3.txt
+git commit -m "add 3.txt"
+
+cd $fork
+touch 4.txt
+git add 4.txt
+git commit -m "add 4.txt"
+
 git fetch origin
 git rebase origin/master
 
